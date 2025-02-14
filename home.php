c946e9bb5f8de9f0e264cc34736781fdf9b09f6f
augale2 <augale2@illinois.edu>
2025-02-14 13:09:20 -0600

    image links changed

--- home.php
+++ home.php
@@ -9,30 +9,30 @@ get_header();
         <div class="flex items-center w-full justify-between">
             <!-- Left Image (Left Half Visible, Right Half Hidden) -->
             <div class="w-1/3 overflow-hidden relative">
-                <img src="https://uillinoisedu-my.sharepoint.com/:i:/r/personal/jlgustin_illinois_edu/Documents/NewMaizeWebsite/107B_Kernels_P1rr_1.jpg?csf=1&web=1&e=FyfCBf"
+                <img src="https://maizecoopsc.web.illinois.edu/wp-content/uploads/2025/02/107B_Kernels_P1rr_1.jpg"
                     alt="Left Image" class="w-[200%] h-auto object-cover -translate-x-1/4">
             </div>
 
             <!-- Right Image (Right Half Visible, Left Half Hidden) -->
             <div class="w-1/3 overflow-hidden relative">
-                <img src="https://uillinoisedu-my.sharepoint.com/:i:/r/personal/jlgustin_illinois_edu/Documents/NewMaizeWebsite/107F_Kernels_P1vv_1.jpg?csf=1&web=1&e=i3EdJf"
+                <img src="https://maizecoopsc.web.illinois.edu/wp-content/uploads/2025/02/107B_Kernels_P1rr_1.jpg"
                     alt="Right Image" class="w-[200%] h-auto object-cover translate-x-1/4">
             </div>
         </div>
 
         <!-- Content Area (Absolute Positioning) -->
         <div class="absolute inset-0 flex justify-center items-center">
-            <div class="bg-white p-10 max-w-2xl text-center shadow-lg rounded-md">
+            <div class="bg-white p-10 max-w-2xl text-center rounded-md">
                 <h1 class="text-3xl font-semibold text-gray-800">Welcome to the Maize Genetics COOP</h1>
                 <p class="mt-4 text-lg text-gray-600 leading-relaxed">
                     The Maize Genetics Cooperation • Stock Center is operated by 
-                    <a href="https://www.ars.usda.gov/" target="_blank" class="text-blue-600 underline">USDA/ARS</a>, located at the 
-                    <a href="https://www.illinois.edu/" class="text-blue-600 underline">University of Illinois, Urbana/Champaign</a>, 
+                    <a href="https://www.ars.usda.gov/" target="_blank" class="underline">USDA/ARS</a>, located at the 
+                    <a href="https://www.illinois.edu/" class="underline">University of Illinois, Urbana/Champaign</a>, 
                     and integrated with the 
-                    <a href="https://www.ars-grin.gov/npgs/" target="_blank" class="text-blue-600 underline">National Plant Germplasm System (NPGS)</a>. 
+                    <a href="https://www.ars-grin.gov/npgs/" target="_blank" class="underline">National Plant Germplasm System (NPGS)</a>. 
                     We serve the maize research community by collecting, maintaining, and distributing seeds of maize genetic stocks. 
                     We also provide information about our stocks and the mutations they carry through the 
-                    <a href="https://www.maizegdb.org/" target="_blank" class="text-blue-600 underline">Maize Genetics and Genomics Database (MaizeGDB)</a>.
+                    <a href="https://www.maizegdb.org/" target="_blank" class="underline">Maize Genetics and Genomics Database (MaizeGDB)</a>.
                 </p>
             </div>
         </div>
